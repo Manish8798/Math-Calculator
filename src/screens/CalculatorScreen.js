81fc7b01844eb3da127ef96121505a911bcdd632
Manish8798 <mankum08071998@gmail.com>
2023-07-30 02:35:01 +0530

    rational number added

--- src/screens/CalculatorScreen.js
+++ src/screens/CalculatorScreen.js
@@ -24,6 +24,14 @@ const CalculatorScreen = props => {
   const [inputCount, setInputCount] = React.useState([1, 2]);
   const [inputValues, setInputValues] = React.useState([]);
   const [result, setResult] = React.useState(null);
+  const [inputRationalNum1, setInputRationalNum1] = React.useState({
+    num1: null,
+    deno1: null,
+  });
+  const [inputRationalNum2, setInputRationalNum2] = React.useState({
+    num2: null,
+    deno2: null,
+  });
 
   const chooseOperation = value => {
     switch (value) {
@@ -32,7 +40,76 @@ const CalculatorScreen = props => {
 
       case 'LCM':
         return lcmView();
+
+      case 'Rational Numbers':
+        return rationalNumberView();
     }
+  };
+
+  const rationalNumberView = () => {
+    return (
+      <View style={{marginTop: 20, paddingHorizontal: 10}}>
+        <View style={styles.inputContainer}>
+          <View style={styles.inputView}>
+            <Input
+              inputContainerStyle={{borderColor: 'transparent'}}
+              style={styles.input}
+              placeholder="numerator 1"
+              value={inputRationalNum1.num1}
+              keyboardType="numeric"
+              onChangeText={text => handleChangeText(text, null, 'num1')}
+            />
+          </View>
+          <View style={styles.inputView}>
+            <Input
+              inputContainerStyle={{borderColor: 'transparent'}}
+              style={styles.input}
+              placeholder="denominator 1"
+              value={inputRationalNum1.deno1}
+              keyboardType="numeric"
+              onChangeText={text => handleChangeText(text, null, 'deno1')}
+            />
+          </View>
+        </View>
+        <View style={styles.inputContainer}>
+          <View style={styles.inputView}>
+            <Input
+              inputContainerStyle={{borderColor: 'transparent'}}
+              style={styles.input}
+              placeholder="numerator 2"
+              value={inputRationalNum2.num2}
+              keyboardType="numeric"
+              onChangeText={text => handleChangeText(text, null, 'num2')}
+            />
+          </View>
+          <View style={styles.inputView}>
+            <Input
+              inputContainerStyle={{borderColor: 'transparent'}}
+              style={styles.input}
+              placeholder="denominator 2"
+              value={inputRationalNum2.deno2}
+              keyboardType="numeric"
+              onChangeText={text => handleChangeText(text, null, 'deno2')}
+            />
+          </View>
+        </View>
+        <Button
+          title={`Answer`}
+          buttonStyle={{
+            backgroundColor: '#1B66C9',
+            borderRadius: 4,
+          }}
+          containerStyle={{
+            width: '50%',
+            marginHorizontal: 5,
+            marginBottom: 20,
+            alignSelf: 'center',
+          }}
+          onPress={() => handleSolution()}
+        />
+        <Text style={styles.resultText}>Result: {result}</Text>
+      </View>
+    );
   };
 
   const handleAddMoreInput = type => {
@@ -53,16 +130,93 @@ const CalculatorScreen = props => {
     });
   };
 
-  const handleChangeText = (text, index) => {
-    let tempArr = inputValues;
-    tempArr[index] = text;
-    setInputValues(tempArr);
+  const handleChangeText = (text, index, type) => {
+    console.log(text, index, type);
+    switch (type) {
+      case 'hcf':
+        let tempArr = inputValues;
+        tempArr[index] = text;
+        setInputValues(tempArr);
+        break;
+
+      case 'lcm':
+        let tempArrLcm = inputValues;
+        tempArrLcm[index] = text;
+        setInputValues(tempArrLcm);
+        break;
+
+      case 'num1':
+        setInputRationalNum1({
+          num1: text,
+          deno1: inputRationalNum1.deno1,
+        });
+        break;
+
+      case 'deno1':
+        setInputRationalNum1({
+          num1: inputRationalNum1.num1,
+          deno1: text,
+        });
+        break;
+
+      case 'num2':
+        setInputRationalNum2({
+          num2: text,
+          deno2: inputRationalNum2.deno2,
+        });
+        break;
+
+      case 'deno2':
+        setInputRationalNum2({
+          num2: inputRationalNum2.num2,
+          deno2: text,
+        });
+        break;
+    }
   };
+
+  // function gcd(a, b) {
+  //   if (b === 0) return a;
+  //   return gcd(b, a % b);
+  // }
+
+  // // Function to find the least common multiple (LCM)
+  // function lcm(a, b) {
+  //   return (a * b) / gcd(a, b);
+  // }
+
+  // Function to find n rational numbers between two fractions (numeratorA/denominatorA and numeratorB/denominatorB)
+  function findNRationalNumbersBetweenFractions(
+    numeratorA,
+    denominatorA,
+    numeratorB,
+    denominatorB,
+    n,
+  ) {
+    if (numeratorA / denominatorA >= numeratorB / denominatorB) {
+      console.error(
+        'Invalid input: the first fraction should be less than the second fraction.',
+      );
+      return [];
+    }
+
+    const commonDenominator = lcm(denominatorA, denominatorB);
+    const stepNumerator = (numeratorB - numeratorA) / (n + 1);
+    const rationalNumbers = [];
+
+    for (let i = 1; i <= n; i++) {
+      const numerator = numeratorA + stepNumerator * i;
+      const denominator = commonDenominator;
+      rationalNumbers.push([numerator, denominator]);
+    }
+
+    return rationalNumbers;
+  }
 
   const handleSolution = inputValues => {
     console.log(inputValues);
     Keyboard.dismiss();
-    const filteredArray = inputValues.filter(item => item !== '');
+    const filteredArray = inputValues?.filter(item => item !== '');
     switch (value?.name) {
       case 'HCF':
         setResult(calculateHCFOfNNumbers(filteredArray));
@@ -71,6 +225,22 @@ const CalculatorScreen = props => {
       case 'LCM':
         setResult(calculateLCM(filteredArray));
         console.log(calculateLCM(filteredArray));
+        break;
+      case 'Rational Numbers':
+        const {num1, deno1} = inputRationalNum1;
+        const {num2, deno2} = inputRationalNum2;
+        setResult(
+          `Rational numbers between, ${num1}/${deno1}, and, ${num2}/${deno2}, are: ${findNRationalNumbersBetweenFractions(
+            num1,
+            deno1,
+            num2,
+            deno2,
+            5,
+          )}`,
+        );
+        console.log(
+          findNRationalNumbersBetweenFractions(num1, deno1, num2, deno2, 5),
+        );
         break;
     }
   };
@@ -156,7 +326,7 @@ const CalculatorScreen = props => {
               style={styles.input}
               value={val[i]}
               inputContainerStyle={{borderColor: 'transparent'}}
-              onChangeText={text => handleChangeText(text, i)}
+              onChangeText={text => handleChangeText(text, i, 'hcf')}
             />
           ))}
         </ScrollView>
@@ -243,7 +413,7 @@ const CalculatorScreen = props => {
               style={styles.input}
               value={val[i]}
               inputContainerStyle={{borderColor: 'transparent'}}
-              onChangeText={text => handleChangeText(text, i)}
+              onChangeText={text => handleChangeText(text, i, 'lcm')}
             />
           ))}
         </ScrollView>
@@ -290,6 +460,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginVertical: 20,
     marginHorizontal: 10,
+  },
+  inputContainer: {
+    flexDirection: 'row', // Align children components horizontally
+    justifyContent: 'center', // Center the children components along the horizontal axis
+    paddingHorizontal: 10,
+  },
+  inputView: {
+    flex: 1, // This allows the inputs to take equal width
   },
 });
 
